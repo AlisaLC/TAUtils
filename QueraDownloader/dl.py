0ddf4f7374ba6470b7dc092dbc3add602ec85340
AlisaLC <ali_sal1381@yahoo.com>
2024-01-01 17:02:18 +0330

    updated downloader

--- QueraDownloader/dl.py
+++ QueraDownloader/dl.py
@@ -19,13 +19,28 @@ def from_persian_digits(num):
     return int(num)
 
 
-def download(HW_ID: int, root_dir: str):
+def download(HW_ID: int, PROBLEM_ID:int, root_dir: str):
     HEADER_DATA = {'cookie': f'session_id={os.environ["QUERA_SESSION_ID"]}'}
     total_pages = 100
     page_counter = 1
     while page_counter <= total_pages:
         url = f'https://quera.org/course/assignments/{HW_ID}/submissions/final?page={page_counter}'
-        r = requests.get(url, headers=HEADER_DATA)
+        if PROBLEM_ID != 0:
+            url += f'&problem={PROBLEM_ID}'
+        try:
+            r = requests.get(url, headers=HEADER_DATA)
+            r.raise_for_status()
+        except requests.exceptions.HTTPError:
+            if r.status_code == 404:
+                print('HW not found!')
+                return
+            if r.status_code == 403:
+                print('Invalid session ID!')
+                return
+            if r.status_code == 429:
+                print('Too many requests! Sleeping for 1 minute...')
+                time.sleep(60)
+                continue
         soup = BeautifulSoup(r.text, features='lxml')
         if page_counter == 1:
             foot = soup.find('tfoot')
@@ -50,14 +65,17 @@ def download(HW_ID: int, root_dir: str):
                 file_r.raise_for_status()
                 try:
                     with zipfile.ZipFile(BytesIO(file_r.content)) as zip_ref:
-                        zip_ref.extractall(f'{root_dir}/{std_id}')
+                        zip_ref.extractall(f'{root_dir}/{(str(PROBLEM_ID) + "/") if PROBLEM_ID != 0 else ""}{std_id}')
                 except zipfile.BadZipFile:
-                    os.makedirs(f'{root_dir}/{std_id}', exist_ok=True)
+                    if PROBLEM_ID != 0:
+                        os.makedirs(f'{root_dir}/{PROBLEM_ID}/{std_id}', exist_ok=True)
+                    else:
+                        os.makedirs(f'{root_dir}/{std_id}', exist_ok=True)
                     file_name = re.search(
                         r'filename="(.+)"', file_r.headers['Content-Disposition']).group(1)
-                    with open(f'{root_dir}/{std_id}/{file_name}', 'wb') as f:
+                    with open(f'{root_dir}/{(str(PROBLEM_ID) + "/") if PROBLEM_ID != 0 else ""}{std_id}/{file_name}', 'wb') as f:
                         f.write(file_r.content)
-                time.sleep(0.5)
+                time.sleep(1)
                 pbar.update(1)
         page_counter += 1
     pbar.close()
@@ -68,7 +86,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Quera HW downloader')
     parser.add_argument('hw_id', type=int,
                         help='HW ID (can be extracted from the url)')
+    parser.add_argument('problem_id', type=int, default=0, nargs='?',
+                        help='Problem ID (can be extracted from the url)')
     parser.add_argument('hw_dir', type=str, default='HW', nargs='?',
                         help='Root directory of downloaded files')
     args = parser.parse_args()
-    download(args.hw_id, args.hw_dir)
+    download(args.hw_id, args.problem_id, args.hw_dir)
